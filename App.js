ba8532edddca3b93efd66d49404ee1bcf27dabe7
eessgg <estergomesdesign@gmail.com>
2020-03-18 10:25:45 -0300

    update folder

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Main from './app/components/Main'
+
+export default function App() {
+  return (  
+    <Main />
+    
+  );
+}
